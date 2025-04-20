d8e7b2acc1702392c9e1751f3bed95b3de9bdc03
Joyce <jzlam@andrew.cmu.edu>
2025-04-20 17:01:58 -0400

    front

--- frontend/app.py
+++ frontend/app.py
@@ -11,7 +11,7 @@ os.chdir(project_root)
 
 from belief_graph import similar_to
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=current_dir)
 
 # Inline HTML template
 TEMPLATE = 'template.html'
@@ -23,7 +23,7 @@ def index():
     if request.method == 'POST':
         query = request.form.get('query', '')
         result = similar_to(query)
-    return render_template(TEMPLATE, result=result, query=query)
+    return render_template('template.html', result=result, query=query)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
